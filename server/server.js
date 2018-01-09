9beaf311724e78a24d99f86224d7c9279258366c
Erick Morales Silva <erick.silva@localhost.localdomain>
2018-01-08 19:29:03 -0600

    Add GET /todos route.

--- server/server.js
+++ server/server.js
@@ -25,6 +25,16 @@ app.post('/todos', (req, res) => {
 
     });
 });
+
+app.get('/todos', (req, res) => {
+
+    Todo.find().then((todos) => {
+        res.send({ todos });
+
+    }).catch((error) => {
+        res.send({todos});
+    });
+});
   
 app.listen(3000, () => {
 

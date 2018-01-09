7e4b6990afc9834e9b4989951d114b3b4c65c6d7
Erick Morales Silva <erick.silva@localhost.localdomain>
2018-01-08 18:19:10 -0600

    Add POST /todos and mongo refactoring.

---
+++ server/db/mongoose.js
@@ -0,0 +1,9 @@
+
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+var uri = 'mongodb://esilva2902:S1lva@gettingstarted-shard-00-00-qiejp.mongodb.net:27017,gettingstarted-shard-00-01-qiejp.mongodb.net:27017,gettingstarted-shard-00-02-qiejp.mongodb.net:27017/TodoApp?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin';
+mongoose.connect(uri);
+
+module.exports = { mongoose }

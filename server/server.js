5cbecd0d68d67288b107c56ed2fbf150e5718bd8
Erick Morales Silva <erick.silva@localhost.localdomain>
2018-11-18 20:34:29 -0600

    v0.11.3 - PATCH /todos/:id route was added.

--- server/server.js
+++ server/server.js
@@ -66,21 +66,48 @@ app.get('/todos/:id', (req, res) => {
 // DELETE /todos/:id:
 app.delete('/todos/:id', (req, res) => {
     let id = req.params.id;
-  
+
     if (!ObjectID.isValid(id)) {
-      return res.status(404).send();
-    }
-  
-    Todo.findByIdAndRemove(id).then((todo) => {
-      if (!todo) {
         return res.status(404).send();
-      }
-  
-      res.send(todo);
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send(todo);
     }).catch((e) => {
-      res.status(400).send();
+        res.status(400).send();
     });
-  });
+});
+
+// PATCH /todos/:id:
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send();
+    })
+});
 
 // POST /users
 app.post('/users', (req, res) => {
